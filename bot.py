66ae5d258e1777b3c07658227de94e9066695197
Vladyslava <vladaslavadulembova@gmail.com>
2025-02-22 18:40:27 +0000

    retry commit first

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
+import asyncio
+import logging
+from aiogram import Bot, types, Dispatcher, executor
+
 
 API_TOKEN = "7951258588:AAHxgaubHfJb01xcyd95HakpLIj1ierIezQ"
 bot = Bot(token=API_TOKEN)
@@ -14,7 +15,8 @@ async def process_start_command(message: types.Message):
 @dp.message_handler()
 async def echo_message(msg: types.Message):
         await bot.send_message(msg.from_user.id, msg.text)
+async def main():
+        await dp.start_polling(bot)
 
-
-        if __name__ == '__main__':
-            executor.start_polling(dp)
+        if __name__ == "__main__":
+            asyncio.run(main())
